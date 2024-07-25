b7b248e9fc8ed6db95e25660cfba99e9858a77cf
Damian Pokorski <damianpokorski@hotmail.com>
2024-07-25 19:40:47 +0000

    feat(semantic-release): initial semantic release setup

---
+++ release.config.mjs
@@ -0,0 +1,17 @@
+export default {
+  branches: ['feature/semantic-release', 'main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: [],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+      }
+    ]
+    // '@semantic-release/github'
+  ]
+};
